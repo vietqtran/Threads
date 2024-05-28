af5689abc0694bded2179d9c25227d8d002bbea3
tranquocviet1303 <tranquocviet1303@gmail.com>
2024-05-28 07:25:11 +0000

    chore: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './common/_app/app.module'
+import { AppModule } from './common/app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   await app.listen(3000)
 }
 bootstrap()
