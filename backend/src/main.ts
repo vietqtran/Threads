118209bef68cd7e98ab314b5c975494ae6a11c23
tranquocviet1303 <tranquocviet1303@gmail.com>
2024-05-28 07:26:30 +0000

    chore: add swagger to project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './common/app/app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('EcomMERN API')
+    .setDescription('EcomMERN API description')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  
+
   await app.listen(3000)
 }
 bootstrap()
