e9c914a4b1d58b7883589d0bae3b8b168c507b68
tranquocviet1303 <tranquocviet1303@gmail.com>
2024-05-29 03:07:16 +0000

    fix: update accept methods

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   
