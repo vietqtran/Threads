580454459ff0c8f45dbbcc9b8d12910e2c6d335a
tranquocviet1303 <tranquocviet1303@gmail.com>
2024-05-29 03:42:52 +0000

    feat: firebase

--- frontend/src/components/common/Test.tsx
+++ frontend/src/components/common/Test.tsx
@@ -3,6 +3,9 @@
 import { API_METHOD } from '@/constants/api-methods'
 import { useCallApi } from '@/hooks'
 import { useEffect } from 'react'
+import { Button } from '../ui/button'
+import { auth, googleProvider } from '@/utils/firebase'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 const Test = () => {
   useEffect(() => {
@@ -15,7 +18,35 @@ const Test = () => {
     handle()
   }, [])
 
-  return <div></div>
+  const handleLogin = async () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result)
+        console.log(credential)
+        // The signed-in user info.
+        const user = result.user
+        console.log(user)
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code
+        const errorMessage = error.message
+        // The email of the user's account used.
+        const email = error.customData.email
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error)
+        // ...
+      })
+  }
+
+  return (
+    <div>
+      <Button onClick={handleLogin}>Login</Button>
+    </div>
+  )
 }
 
 export default Test
