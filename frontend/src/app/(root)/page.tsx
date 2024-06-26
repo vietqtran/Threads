08e5467b1beb531eb9c33377371e726ab3e75a94
vietqtran <tranquocviet1303@gmail.com>
2024-06-26 22:21:51 +0700

    home section

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,19 +1,50 @@
-import { getUsers } from '@/hooks/useUsers'
-import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query'
+import Icon from '@/components/Common/Icon'
 
-export default async function Home() {
-  const queryClient = new QueryClient()
-
-  await queryClient.prefetchQuery({
-    queryKey: ['posts'],
-    queryFn: getUsers
-  })
-
+export default function Home() {
   return (
-    <>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="pl-[100px]"></div>
-      </HydrationBoundary>
-    </>
+    <div className="flex h-full w-full justify-center px-[76px]">
+      <div className="h-full w-[640px]">
+        <div className="grid h-15 w-full place-items-center">
+          <div className="flex items-center gap-2">
+            <span className="block text-15px font-medium">For you</span>
+            <div className="relative">
+              <div className="grid h-[24px] w-[24px] cursor-pointer place-items-center rounded-full border bg-content shadow duration-75 ease-linear hover:scale-110">
+                <Icon name="arrow_down_home_section_black" className="dark:hidden" size={12} />
+                <Icon name="arrow_down_home_section_white" className="hidden dark:block" size={12} />
+              </div>
+              <div className="absolute left-1/2 top-[calc(100%+6px)] w-[224px] -translate-x-1/2 rounded-2xl border p-2 shadow">
+                <div className="cursor-pointer rounded-xl p-3 hover:bg-content-hover">
+                  <div className="flex h-7 w-full items-center justify-between">
+                    <span className="font-medium">For you</span>
+                    <div>
+                      <Icon name="check_home_select_section_black" className="dark:hidden" size={16} />
+                      <Icon name="check_home_select_section_white" className="hidden dark:block" size={16} />
+                    </div>
+                  </div>
+                </div>
+                <div className="cursor-pointer rounded-xl p-3 hover:bg-content-hover">
+                  <div className="flex h-7 w-full items-center justify-between">
+                    <span className="font-medium">Following</span>
+                    <div></div>
+                  </div>
+                </div>
+                <div className="cursor-pointer rounded-xl p-3 hover:bg-content-hover">
+                  <div className="flex h-7 w-full items-center justify-between">
+                    <span className="font-medium">Liked</span>
+                    <div></div>
+                  </div>
+                </div>
+                <div className="cursor-pointer rounded-xl p-3 hover:bg-content-hover">
+                  <div className="flex h-7 w-full items-center justify-between">
+                    <span className="font-medium">Saved</span>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/components/Common/Layout/MainLayout.tsx
+++ frontend/src/components/Common/Layout/MainLayout.tsx
@@ -3,14 +3,10 @@ import React from 'react'
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full w-full">
-      <div className="">
+    <div className="h-screen w-screen">
+      <div className="size-full">
         <Header />
         {children}
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoRGyXmHy_6aIgXYqWHdOT3KjfmnuSyxypw&s"
-          alt=""
-        />
       </div>
     </div>
   )
